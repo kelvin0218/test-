afff875d78073168ece5dc39b0eb299469c72607
kelvin0218 <kelvin19960218@gmail.com>
2017-06-01 14:36:34 +0800

    initial commit 2

---
+++ uthink tutorial UI 1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  uthink tutorial UI 1
+//
+//  Created by Kelvin Tse on 1/6/2017.
+//  Copyright © 2017年 Kelvin Tse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       /* let circlePath = UIBezierPath(arcCenter: CGPoint(x: 100,y: 100), radius: CGFloat(20), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        //change the fill color
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        //you can change the stroke color
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        //you can change the line width
+        shapeLayer.lineWidth = 3.0
+        
+        view.layer.addSublayer(shapeLayer)
+    */
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
